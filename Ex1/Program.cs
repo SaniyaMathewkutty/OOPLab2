1b31a05e82f9e422cf3231a3cfa377d29182ec98
S00223097 <S00223097@LAB-B1042-29>
2022-09-26 10:19:01 +0100

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+    Name: Saniya Mathewkutty
+    Date: 26/09/2022
+    Description: Lab 1, Exercise 1
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+        }
+    }
+}
