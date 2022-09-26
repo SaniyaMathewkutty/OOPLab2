4431651c5b57bc4b7158e1a55ba122f993e41820
Saniya Mathewkutty <S00223097@atu.ie>
2022-09-26 12:24:49 +0100

    Edits to Ex3

--- Ex3/Ex3.cs
+++ Ex3/Ex3.cs
@@ -3,11 +3,11 @@
     Date: 26/09/2022
     Description: Lab 1, Exercise 3: Array
 
-Create two arrays which hold the grade boundaries and higher level point.
-The grade boundaries are the marks that are on the boundary between two grades
-such as 90, 80, 70 etc. Amend the program so that it makes use of the arrays
-in calculating points. This program should read from a file but should output
-total points to the screen and not amend the file.
+    Create two arrays which hold the grade boundaries and higher level point.
+    The grade boundaries are the marks that are on the boundary between two grades
+    such as 90, 80, 70 etc. Amend the program so that it makes use of the arrays
+    in calculating points. This program should read from a file but should output
+    total points to the screen and not amend the file.
 
 */
 
@@ -29,7 +29,7 @@ namespace Lab2
             string filePath = "results.txt";
             string[] fileContents = File.ReadAllLines(filePath); // array containing all the text in the file
         
-            // Call the method to calculate points from the contents in the file
+            // Call the method that calculates points from the contents in the file
             int totalPoints = CalculatePoints(fileContents);
 
             // Print output to console
@@ -48,10 +48,26 @@ namespace Lab2
             // Calculate total points
             int totalPoints = 0, points = 0, result = 0;
 
-            for (int i = 0; i < data.Length; i++)
+            for (int i = 0; i < data.Length; i++) // loop through data
             {
+                result = int.Parse(data[i]);
+
+                // Loop through boundaries
+                for (int j = 0; j < gradeBoundaries.Length; j++)
+                {
+                    if (result >= gradeBoundaries[j]) // if the result in this iteration (i) is greater than or equal the grade boundaries iteration (j)... 
+                    {
+                        points=higherPoints[j];
+                        break; // Break out of the loop when this check has been completed
+                    }
+                }
+
+                // Increment total points by the points in the current iteration
+                totalPoints += points;
 
             }
+
+            return totalPoints; 
         }
     }
 }
