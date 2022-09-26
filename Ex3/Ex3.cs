6cf534de08229bdc5b9adc4ad43c7ff0abbadb89
Saniya Mathewkutty <S00223097@atu.ie>
2022-09-26 11:53:12 +0100

    edits

--- Ex3/Ex3.cs
+++ Ex3/Ex3.cs
@@ -1,10 +1,28 @@
-﻿namespace Ex3
+﻿/*
+    Name: Saniya Mathewkutty
+    Date: 26/09/2022
+    Description: Lab 1, Exercise 2: Foreach statement with file handling
+
+Read a text file which contains 7 grades. Calculate the total points and append this 
+total to the original file. Don’t worry about Higher or Ordinary level here but use 
+the points for Higher.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ex3
 {
-    internal class Ex3
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
