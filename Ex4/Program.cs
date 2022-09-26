add411a84380b99a51ffe0fbe9f4f0b9fc247aad
Saniya Mathewkutty <S00223097@atu.ie>
2022-09-26 12:37:52 +0100

    Ex4

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Ex4
+﻿/*
+    Name: Saniya Mathewkutty
+    Date: 26/09/2022
+    Description: Lab 1, Exercise 4:
+
+    Create a method which accepts an array of grades 
+    (assume higher level only) and calculates the points.
+*/
+namespace Ex4
 {
     class Program
     {
