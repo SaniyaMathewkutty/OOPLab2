0d52efde6a22738a025ff11c55083211df0f496e
SaniyaMathewkutty <98536910+SaniyaMathewkutty@users.noreply.github.com>
2022-10-01 01:32:38 +0100

    Ex5

--- Ex5/Ex5.cs
+++ Ex5/Ex5.cs
@@ -1,4 +1,6 @@
-﻿namespace Ex5
+﻿using System.Globalization;
+
+namespace Ex5
 {
     class Program
     {
@@ -31,8 +33,73 @@
                 results[i] = Console.ReadLine();
             }
 
+            // Total Points method calling 
+            int totalPoints = CalculatePoints(results, levels, points);
+
+            // Display results
+            
 
 
+
+        }
+
+        private static int CalculatePoints(string[] data, string[] levels, int[] studentPoints)
+        {
+            // Variables for calculating total
+            int totalPoints = 0, points = 0, result = 0;
+
+            // Initialise arrays for grade boundaries and higher level points
+            int[] gradeBoundaries = new int[8] { 90, 80, 70, 60, 50, 40, 30, 0 };
+            int[] higherPoints = new int[8] { 100, 88, 77, 66, 56, 46, 37, 0 };
+
+            // Loop through grades in file 
+            for (int i = 0; i < data.Length; i++)
+            {
+                result = int.Parse(data[i]); // Convert i'th number from file into int
+
+                // Loop through boundaries as well
+                for (int j = 0; j < gradeBoundaries.Length; j++)
+                {
+                    if (result >= gradeBoundaries[j]) // is the result greater/equal to to values in grade boundaries array
+                    {
+                        // Higher or ordinary level check
+                        points = levels[i].ToLower().Equals("h") ? higherPoints[j] : ordinaryPoints[j];
+                        break; // break out of nested loop at this point
+                    }
+                }
+
+                studentPoints[i] = points; // Not totalPoints...
+            }
+
+            // Determining the Top 6 for points calc
+            Array.Sort(studentPoints); // lowest first
+            Array.Reverse(studentPoints); // highest first
+
+            // Loop through top 6 and add to totalPoints
+            for (int i = 0; i < 6; i++)
+            {
+                totalPoints += studentPoints[i];
+            }
+
+            return totalPoints;
+        }
+    
+        private static void DisplayPoints(string name, StringInfo studentNo, string[] results, string[] subjects, string[] levels, int[] points, int totalPoints)
+        {
+            Console.Write("Name :  {0}", name);
+            Console.Write("Student Number : {0}", studentNo);
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                Console.Write($"{subjects[i],20} {levels[i],13} {results[i], 13} {points[i], 13});
+            }
+
+            Console.WriteLine("Total points = {0}", totalPoints);
+        }
+   
+        private static void WriteInfoToFile(string name)
+        {
+            // Stream writer to write to file 
 
         }
     }
